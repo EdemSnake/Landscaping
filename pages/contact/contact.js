f684439a5eb177728703138e2d597e748056d252
Edem <bewareships@gmail.com>
2024-11-24 22:54:00 +0000

    contact page

---
+++ pages/contact/contact.js
@@ -0,0 +1,61 @@
+const navbar = document.querySelector(".navbar");
+const content = document.querySelector(".main-content-contact");
+const foot = document.querySelector(".footer");
+
+const checkbox = document.querySelector("#terms");
+const submitButton = document.querySelector("#submit");
+
+content.style.marginTop = `${navbar.offsetHeight}px`;
+content.style.marginBottom = `${foot.offsetHeight}px`;
+
+const counties = [
+  "Antrim",
+  "Armagh",
+  "Carlow",
+  "Cavan",
+  "Clare",
+  "Cork",
+  "Derry",
+  "Donegal",
+  "Down",
+  "Dublin",
+  "Fermanagh",
+  "Galway",
+  "Kerry",
+  "Kildare",
+  "Kilkenny",
+  "Laois",
+  "Leitrim",
+  "Limerick",
+  "Longford",
+  "Louth",
+  "Mayo",
+  "Meath",
+  "Monaghan",
+  "Offaly",
+  "Roscommon",
+  "Sligo",
+  "Tipperary",
+  "Tyrone",
+  "Waterford",
+  "Westmeath",
+  "Wexford",
+  "Wicklow",
+];
+
+const dropdown = document.getElementById("dropdown-county");
+
+counties.forEach((county) => {
+  const option = document.createElement("option");
+  option.value = county;
+  option.textContent = county;
+  if (county === "Dublin") {
+    option.selected = true;
+  }
+  dropdown.append(option);
+});
+
+// eslint-disable-next-line no-unused-vars
+function toggleCheckbox() {
+  submitButton.disabled = !checkbox.checked;
+}
